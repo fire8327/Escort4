73056670001a35114e5accb7ec1d94caeea686cc
fire8327 <fire83274@gmail.com>
2024-08-15 15:24:43 +0300

    upd

---
+++ Assets/JS/main.js
@@ -0,0 +1,29 @@
+/* showPhone */
+$(".showPhone").each((i, el) => {
+    $(el).click(() => {
+        $(el).find(".show").addClass("hidden")
+        $(el).find(".phone").removeClass("hidden")
+    })
+})
+
+
+/* tabs */
+$(".tab-main").each(function(index) {
+    let $this = $(this);
+    let prefix = "tabs-" + index;
+  
+    $this.find("ul a").each(function(tabIndex) {
+      let $tab = $(this);
+      let tabId = prefix + "-tab-" + tabIndex;
+      let panelId = prefix + "-panel-" + tabIndex;
+  
+      $tab.attr("href", "#" + panelId);
+      $tab.closest("li").attr("id", tabId);
+    });
+  
+    $this.find(".tab-card").each(function(panelIndex) {
+      $(this).attr("id", prefix + "-panel-" + panelIndex);
+    });
+  
+    $this.tabs();
+});
